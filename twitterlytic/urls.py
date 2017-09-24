1bfdf91bcf31d425b035491289b32b00ec0017bd
Madeleine Price Ball <mpball@gmail.com>
2017-09-24 13:49:51 -0700

    Initialize templates, styles

--- twitterlytic/urls.py
+++ twitterlytic/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from django.views.generic import TemplateView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$',
+        TemplateView.as_view(template_name='twitterlytic/index.html'),
+        name='home'),
 ]
